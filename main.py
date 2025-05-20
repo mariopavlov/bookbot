bac20346eb02539322916f39ec5636d0ec2cc450
Mario Pavlov <marioppavlov@gmail.com>
2025-05-20 20:39:31 +0300

    Implement Word Counting

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        
+
         return file_contents
+
+def count_words(text):
+    number_of_words = text.split()
+
+    return len(number_of_words)
+
 
 def main():
     frankenstein_book = "books/frankenstein.txt"
 
     content = get_book_text(frankenstein_book)
+    num_words = count_words(content)
 
-    print(content)
+    print(f"{num_words} words found in the document")
+
 
 if __name__ == "__main__":
     main()
-
