2b519f8e2f1e1511f14891db3244ad4ab687c23b
Mario Pavlov <marioppavlov@gmail.com>
2025-05-21 12:38:51 +0300

    Implement Sort Function for Character List
    Implemented Sorting function that accepts character list and returns
    sorted list of characters.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_characters, count_words
+from stats import count_characters, count_words, sort_characters
+from curses.ascii import isalpha
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,6 +7,21 @@ def get_book_text(file_path):
 
         return file_contents
 
+def print_report(book, num_words, sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for single_character in sorted_characters:
+
+        if single_character["char"].isalpha():
+            char = single_character["char"]
+            count = single_character["num"]
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 def main():
     frankenstein_book = "books/frankenstein.txt"
@@ -13,11 +29,14 @@ def main():
     content = get_book_text(frankenstein_book)
 
     num_words = count_words(content)
-    print(f"{num_words} words found in the document")
+    # print(f"{num_words} words found in the document")
 
     num_characters = count_characters(content)
-    print(num_characters)
+    # print(num_characters)
 
+    sorted_characters = sort_characters(num_characters)
+
+    print_report(frankenstein_book, num_words, sorted_characters)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def count_words(text):
 
     return len(number_of_words)
 
-def count_characters(text):
+def count_characters(text: str):
     all_characters = {}
 
     for word in text.split():
@@ -16,3 +16,28 @@ def count_characters(text):
                 all_characters[char] = 1
 
     return all_characters
+
+def sort_characters(characters):
+    """
+    Combines characters and character counts into single dictionary.
+
+    Parameters:
+    characters (dictionary): All characters and their count
+
+    Returns:
+    dictionary: sorted dictionary of all characters. Sorted by count ocurence
+
+    """
+    character_list = []
+
+    for char, count in characters.items():
+        single_char = {
+            "char": char,
+            "num": count
+        }
+
+        character_list.append(single_char)
+
+    sorted_characters = sorted(character_list, key=lambda x: x["num"], reverse=True)
+
+    return sorted_characters
